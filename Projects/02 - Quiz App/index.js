7121e59c40dbd779424bcf69688ed355a1ac9476
Koech-sophronius- <123469961+Sophinius-comp-os@users.noreply.github.com>
2024-03-29 21:07:07 +0300

    modified the files

--- Projects/02 - Quiz App/index.js
+++ Projects/02 - Quiz App/index.js
@@ -1,4 +1,4 @@
-const questions = [
+const questions = [...
   {
     question: "What is the capital of France?",
     choices: [
@@ -90,3 +90,87 @@ const questions = [
     ],
   },
 ];
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-button");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+const startQuiz = () => {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+};
+
+const showQuestion = () => {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNumber = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
+
+  currentQuestion.choices.forEach((choice) => {
+    const button = document.createElement("button");
+    button.innerHTML = choice.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (choice.answer) {
+      button.dataset.answer = choice.answer;
+    }
+    button.addEventListener("click", selectChoice);
+  });
+};
+
+const resetState = () => {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+};
+
+const selectChoice = (e) => {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.answer === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.answer === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+
+  nextButton.style.display = "block";
+};
+
+const showScore = () => {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+};
+
+const handleNextButton = () => {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+};
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+startQuiz();
